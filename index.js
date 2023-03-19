0aa0e00328085d2dd00ccd2c8d63c87c3d588d6f
Taylor Weirtz <teweirtz@gmail.com>
2023-03-19 15:42:17 -0400

    added middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import cocktailRouter from "./routes/cocktails.js"
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Shaken, not stirred');
 });
